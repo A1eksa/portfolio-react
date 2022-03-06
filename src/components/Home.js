90f16f066d5afde4aafd7d1105f2e6ed36a0a05a
Aleksandra Safranko <aleksandra.safranko1@gmail.com>
2022-03-07 00:11:38 +0100

    started creating home

---
+++ src/components/Home.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Home.css';
+import styled from 'styled-components';
+
+export const Home = () => {
+  return (
+    <Wrapper>
+      <Name>Aleksandra Safranko</Name>
+      <Title>Frontend developer</Title>
+      <SpanContainer>
+        <div className='span-container'>
+          <p>
+            I <span className='change-text'></span>{' '}
+            <span className='responsive'>b</span>
+            <span className='responsive'>e</span>
+            <span className='responsive'>a</span>
+            <span className='responsive'>u</span>
+            <span className='responsive'>t</span>
+            <span className='responsive'>i</span>
+            <span className='responsive'>f</span>
+            <span className='responsive'>u</span>
+            <span className='responsive'>l</span>
+            <span className='responsive'>l</span>{' '}
+            <span className='space'>and</span>{' '}
+            <span className='responsive'>r</span>
+            <span className='responsive'>e</span>
+            <span className='responsive'>s</span>
+            <span className='responsive'>p</span>
+            <span className='responsive'>o</span>
+            <span className='responsive'>n</span>
+            <span className='responsive'>s</span>
+            <span className='responsive'>i</span>
+            <span className='responsive'>v</span>
+            <span className='responsive'>e</span> websites.
+          </p>
+        </div>
+      </SpanContainer>
+    </Wrapper>
+  );
+};
+
+export const Title = styled.h1`
+  font-size: 4rem;
+`;
+
+export const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SpanContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+`;
+
+export const Name = styled.h2`
+  font-size: 3rem;
+`;
